0485742bb66ee582fd7c1a3710eaeba79deb2694
Nick@Nucbox <rustycloud42@protonmail.com>
2025-07-08 09:19:53 -0600

    feat: Compresses executables in postbuild job, attempts decompression of executable in post-install.js

---
+++ config.ts
@@ -0,0 +1,63 @@
+export default {
+  build: {
+    sources: {
+      cli: {
+        main: "lib/cli.tsx",
+        worker: "lib/_worker.tsx",
+      },
+      exports: {
+        server: "lib/exports/server.ts",
+        browser: "lib/exports/browser.ts",
+      },
+    },
+    targets: [
+      {
+        name: "waavy-linux-x64-modern",
+        target: "bun-linux-x64-modern",
+        platform: "Linux x64 (modern)",
+      },
+      {
+        name: "waavy-linux-x64-baseline",
+        target: "bun-linux-x64-baseline",
+        platform: "Linux x64 (baseline)",
+      },
+      {
+        name: "waavy-macos-x64",
+        target: "bun-darwin-x64",
+        platform: "macOS x64",
+      },
+      {
+        name: "waavy-macos-arm64",
+        target: "bun-darwin-arm64",
+        platform: "macOS ARM64",
+      },
+      {
+        name: "waavy-linux-arm64",
+        target: "bun-linux-arm64",
+        platform: "Linux ARM64",
+      },
+      {
+        name: "waavy-windows-x64-modern",
+        target: "bun-windows-x64-modern",
+        platform: "Windows x64 (modern)",
+      },
+      {
+        name: "waavy-windows-x64-baseline",
+        target: "bun-windows-x64-baseline",
+        platform: "Windows x64 (baseline)",
+      },
+    ],
+    defaultBuildOptions: (external: string[], outdir: string) => ({
+      external,
+      outdir,
+      minify: true,
+      sourcemap: "linked" as const,
+      splitting: false,
+      root: "./lib",
+      packages: "bundle" as const,
+      define: {
+        "process.env.NODE_ENV": '"production"',
+      },
+    }),
+  },
+};
---
+++ scripts/gzip-binaries.ts
@@ -0,0 +1,43 @@
+import debug from "debug";
+import { createReadStream, createWriteStream } from "fs";
+import path from "path";
+import { performance } from "perf_hooks";
+import { createGzip } from "zlib";
+import { pipeline } from "stream/promises";
+import config from "../config";
+
+const log = debug("waavy:compress:gzip");
+const outdir = path.join(process.cwd(), "out");
+
+const executables = config.build.targets.map((t) =>
+  path.resolve(outdir, t.name)
+);
+
+async function compress() {
+  await Promise.all(
+    executables.map(async function (executable) {
+      const gzip = createGzip({ level: 9, memLevel: 9 });
+      const filepath = executable.includes("windows") ? executable + ".exe" : executable;
+      const gzipPath = `${filepath}.gz`;
+      const input = createReadStream(filepath);
+      const output = createWriteStream(gzipPath);
+      return await pipeline(input, gzip, output);
+    })
+  );
+}
+
+const start = performance.now();
+
+compress()
+  .then(() =>
+    log(
+      "Compression completed successfully in %d ms",
+      Math.round(performance.now() - start)
+    )
+  )
+  .catch((error) => {
+    const err = log.extend("error");
+    err("Compression failed:", error);
+    err("Failed in %d ms", Math.round(performance.now() - start));
+    process.exit(1);
+  });
