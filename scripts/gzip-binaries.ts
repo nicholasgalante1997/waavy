8d402122934aa49296fb6663db68b1aa6bd48800
Nick@Nucbox <rustycloud42@protonmail.com>
2025-07-30 21:51:33 -0600

    feat: Adds file checking to gzip script

--- scripts/gzip-binaries.ts
+++ scripts/gzip-binaries.ts
@@ -1,22 +1,19 @@
 import debug from "debug";
 import { createReadStream, createWriteStream } from "fs";
+import fs, { access, stat } from "fs/promises";
 import path from "path";
 import { performance } from "perf_hooks";
 import { createGzip } from "zlib";
 import { pipeline } from "stream/promises";
 import config from "@pkg/config";
 
-/**
- * NOTE This delay is for the github actions runner so that the build process has time to release the file handle lock
- */
-await Bun.sleep(1000);
-
 const log = debug("waavy:compress:gzip");
 const outdir = path.join(process.cwd(), "out", "executables");
 const executables = config.build.targets.map((t) => path.resolve(outdir, t.name));
 const start = performance.now();
 
-compressRenderCommandExecutables()
+await waitForFiles();
+await compressRenderCommandExecutables()
   .then(() => log("Compression completed successfully in %d ms", Math.round(performance.now() - start)))
   .catch((error) => {
     const err = log.extend("error");
@@ -36,4 +33,39 @@ async function compressRenderCommandExecutables() {
       return await pipeline(input, gzip, output);
     }),
   );
+}
+
+async function waitForFiles() {
+  const files = [...executables];
+  while (files.length > 0) {
+    log(`Waiting for ${files.length} files to be ready...`);
+    
+    const file = files.shift()!;
+    const filename = file.includes("windows") ? `${file}.exe` : file;
+
+    log(`Waiting for ${file} to be ready...`);
+    try {
+      await access(filename, fs.constants.R_OK);
+      log(`Found ${filename}!`);
+    } catch (error) {
+      console.warn(`Unable to access file: ${filename}`);
+      console.error(`Encountered error: ${error}`);
+      await Bun.sleep(100);
+      files.push(file);
+    }
+
+    try {
+      const stats = await stat(filename);
+      log(`File size: ${stats.size} bytes`);
+      if (stats.size === 0) {
+        console.warn(`${filename} is empty, continuing`);
+        throw new Error(`Missing ${filename}`);
+      }
+    } catch(e) {
+      console.warn(`Unable to stat file: ${filename}`);
+      console.error('Encountering error: ' + e);
+      await Bun.sleep(100);
+      files.push(file);
+    }
+  }
 }
