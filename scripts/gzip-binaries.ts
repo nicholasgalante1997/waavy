8a4ba338f2f28252166ce327fa0853a44273564a
Nick@Nucbox <rustycloud42@protonmail.com>
2025-07-31 15:02:46 -0600

    fix: Makes gzip postbuild action more resilient, but im sure these fucking github runners will find a way through my efforts

--- scripts/gzip-binaries.ts
+++ scripts/gzip-binaries.ts
@@ -7,13 +7,14 @@ import { createGzip } from "zlib";
 import { pipeline } from "stream/promises";
 import config from "@pkg/config";
 
+const MAX_FAILURES = 81;
 const log = debug("waavy:compress:gzip");
 const outdir = path.join(process.cwd(), "out", "executables");
 const executables = config.build.targets.map((t) => path.resolve(outdir, t.name));
 const start = performance.now();
 
-await waitForFiles();
-await compressRenderCommandExecutables()
+await waitForFiles()
+  .then(compressRenderCommandExecutables)
   .then(() => log("Compression completed successfully in %d ms", Math.round(performance.now() - start)))
   .catch((error) => {
     const err = log.extend("error");
@@ -36,22 +37,35 @@ async function compressRenderCommandExecutables() {
 }
 
 async function waitForFiles() {
+  let failures = 0;
   const files = [...executables];
-  while (files.length > 0) {
+  while (files.length > 0 && failures < MAX_FAILURES) {
     log(`Waiting for ${files.length} files to be ready...`);
-    
+
     const file = files.shift()!;
     const filename = file.includes("windows") ? `${file}.exe` : file;
 
     log(`Waiting for ${file} to be ready...`);
+
+    let errors = [];
+    let failed = {
+      access: false,
+      stat: false,
+    };
+
     try {
       await access(filename, fs.constants.R_OK);
       log(`Found ${filename}!`);
     } catch (error) {
       console.warn(`Unable to access file: ${filename}`);
       console.error(`Encountered error: ${error}`);
-      await Bun.sleep(100);
-      files.push(file);
+      errors.push({
+        filename,
+        fn: "fs.access",
+        message: `Unable to access file: ${filename}`,
+        error: error,
+      });
+      failed.access = true;
     }
 
     try {
@@ -61,11 +75,38 @@ async function waitForFiles() {
         console.warn(`${filename} is empty, continuing`);
         throw new Error(`Missing ${filename}`);
       }
-    } catch(e) {
-      console.warn(`Unable to stat file: ${filename}`);
-      console.error('Encountering error: ' + e);
+    } catch (error) {
+      console.warn(`Unable to stat file: ${file}`);
+      console.error("Encountering error: " + error);
+      failed.stat = true;
+      errors.push({
+        filename,
+        fn: "fs.stat",
+        message: `Unable to stat file: ${filename}`,
+        error: error,
+      });
+    }
+
+    if (failed.access) {
+      console.warn(`failed (fs.access): ${filename}`);
+      log(`Failed to access file: ${filename}`);
+    }
+
+    if (failed.stat) {
+      console.warn(`failed (fs.stat): ${filename}`);
+      log(`Failed to stat file: ${filename}`);
+    }
+
+    if (failed.access && failed.stat) {
+      console.warn(`Failed to access and stat file: ${filename}`);
+      console.error(`${filename} is unreachable`);
+      failures += 1;
       await Bun.sleep(100);
       files.push(file);
     }
+  }
+
+  if (failures >= MAX_FAILURES) {
+    throw new Error("HIT MAX FAILURE THRESHOLD");
   }
 }
